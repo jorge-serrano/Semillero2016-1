086b0b5dccf9061ebf77616f611d14cb169c752d
d.andresrojas.h@gmail.com <d.andresrojas.h@gmail.com>
2016-03-16 11:31:11 -0500

    09 Generics update

---
+++ Duban/Generics/Generics/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class Customer
+    {
+        //public int CustomerId { get; set; }
+
+        public string Mail { get; set; }
+
+        public string  Name{ get; set; }
+
+        public Stack<Order> Orders { get; set; }
+
+        public Customer()
+        {
+            Orders = new Stack<Order>();
+        }
+    }
+}
---
+++ Duban/Generics/Generics/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Generics
+{
+    public partial class Form1 : Form
+    {
+        private List<Customer> customers;
+        private List<Order> orders;
+        Messenger msg = new Messenger();
+ 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (Customer customer in customers)
+            {
+                foreach (Order order in orders)
+                    customer.Orders.Push(order);
+                while (customer.Orders.Count > 0)
+                {
+                    var currentOrder = customer.Orders.Pop();
+                    
+                    msg.SendMessage(customer.Mail, CreateMessage(currentOrder));
+                }
+            }
+        }
+
+        private string CreateMessage(Order currentOrder)
+        {
+            return "Your Order " +
+                   currentOrder.ProductDescription + Environment.NewLine +
+                   "Will be sent soon";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            TextFileDataAccess data = new TextFileDataAccess();
+
+            try
+            {
+                customers = data.GetCustomers(@"C:\Users\YUXI\Documents\Visual Studio 2013\Projects\Generics\Generics\Properties\DataSources\");
+                orders = data.GetOrders(@"C:\Users\YUXI\Documents\Visual Studio 2013\Projects\Generics\Generics\Properties\DataSources\");
+                customerBindingSource.DataSource = customers;
+                orderBindingSource.DataSource = orders;
+            }
+            catch (Exception)
+            {
+                lblexcepcion.Text = "File not found";
+            }
+           
+
+        }
+    }
+}
