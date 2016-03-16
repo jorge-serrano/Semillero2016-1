0e0ec671a1c7fb9f64d4b57a88d425ed7016ea54
moisesd37 <moisesddc@hotmail.com>
2016-03-16 08:27:32 -0500

    #3.0.3 added refactoring for generics

---
+++ Moises Dominguez/Generics/Generics/DataAccess.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Generics
+{
+    class DataAccess
+    {
+        public List<Customer> GetCustomers(string folderName)
+        {
+            var file = OpenFile(folderName, "customers.txt");
+            var customersList = new List<Customer>();
+            while (!file.EndOfStream)
+            {
+                string line = file.ReadLine();
+                string[] fields = SplitLine(line);
+                if (customersList.Exists(x=>x.Email==fields[1]))
+                    continue;
+                customersList.Add(new Customer()
+                {
+                    customerId = int.Parse(fields[0]),
+                    Email = fields[1],
+                    Name = fields[2]
+                });
+            }
+            return customersList;
+        }
+        public List<Order> GetOrders(string folderName)
+        {
+            var file = OpenFile(folderName, "orders.txt");
+            var ordersList = new List<Order>();
+            while (!file.EndOfStream)
+            {
+                string line = file.ReadLine();
+                string[] fields = SplitLine(line);
+                ordersList.Add(new Order()
+                {
+                    Quantity = short.Parse(fields[0]),
+                    Description = fields[1],
+                    Deadline = short.Parse(fields[2])
+                });
+            }
+            return ordersList;
+        }
+        private string[] SplitLine(string line)
+        {
+            return line.Split(',');
+        }
+        private StreamReader OpenFile(string folderName, string fileName)
+        {
+            return File.OpenText(folderName + "\\" + fileName);
+        }
+    }
+}
---
+++ Moises Dominguez/Generics/Generics/Hermes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace Generics
+{
+    class Hermes
+    {
+        readonly SmtpClient mailClient = new SmtpClient("smtp.gmail.com", 587);
+        public Hermes()
+        {
+            mailClient.Credentials = new NetworkCredential("moises.dominguez@yuxipacific.com", "Password$4");
+            mailClient.EnableSsl = true;
+        }
+        public bool SendMessage(string AddressTo, string MessageToSend)
+        {
+            MailMessage message = new MailMessage("moises.dominguez@yuxipacific.com", AddressTo, "Your Order is ready", MessageToSend);
+            try {
+                mailClient.Send(message);
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
