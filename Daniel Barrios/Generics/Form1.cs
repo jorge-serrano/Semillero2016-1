2cf21eb539c53f4978a7d7a2b3c93dc54bee9a43
Daniel Barrios Castilla <daniel.barrios@yuxipacific.com>
2016-03-16 15:01:21 -0500

    #26 send refactoring for your collection code

---
+++ Daniel Barrios/Generics/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Generics
+{
+    public partial class Form1 : Form
+    {
+        private List<Customer> customers;
+        private List<Order> orders;
+ 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (Customer customer in customers)
+            {
+                foreach (Order order in orders)
+                    customer.Orders.Push(order);
+                while (customer.Orders.Count > 0)
+                {
+                    var currentOrder = customer.Orders.Pop();
+                    Messenger msg = new Messenger();
+                    msg.SendMessage(customer.Mail, CreateMessage(currentOrder));
+                }
+            }
+        }
+
+        private string CreateMessage(Order currentOrder)
+        {
+            return "Your Order " +
+                   currentOrder.ProductDescription + Environment.NewLine +
+                   "Will be sent soon";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TextFileDataAccess data = new TextFileDataAccess();
+            customers = data.GetCustomers(@"D:\Semillero\Labs\ecommerce");
+            orders = data.GetOrders(@"D:\Semillero\Labs\ecommerce");
+            customerBindingSource.DataSource = customers;
+            orderBindingSource.DataSource = orders;
+
+        }
+    }
+}
---
+++ Daniel Barrios/Generics/Messenger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+
+namespace Generics
+{
+    public class Messenger
+    {
+        readonly SmtpClient mailClient = new SmtpClient("smtp.gmail.com",587);
+
+        public Messenger()
+        {
+            mailClient.Credentials = new NetworkCredential("yegs95@gmail.com","matias1995");
+            //mailClient.UseDefaultCredentials = false;
+            mailClient.EnableSsl = true;
+        }
+        public bool SendMessage(string AddressTo, string MessageToSend)
+        {
+            MailMessage message = new MailMessage("yegs95@gmail.com",AddressTo,"Your Order is ready",MessageToSend);
+            mailClient.Send(message);
+            return true;
+        }
+    }
+}
