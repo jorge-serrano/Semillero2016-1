96ee3c314c4655fc79d55f5d1ae8d5337379e370
d.andresrojas.h@gmail.com <d.andresrojas.h@gmail.com>
2016-03-31 15:36:03 -0500

    #49 DBECommerce Login

---
+++ Duban/DBCommerceLogin/DBCommerceLogin/Index.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DBCommerceLogin
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection url = new SqlConnection();
+                url.ConnectionString = @"Data Source=YUXI\SQLEXPRESS;Initial Catalog=ECommerce;Integrated Security=True";
+                url.Open();
+                SqlCommand GetUser = new SqlCommand("ValidateUser", url);
+                GetUser.CommandType = CommandType.StoredProcedure;
+                GetUser.Parameters.AddWithValue("@userName", txtUser.Text.Trim());
+                GetUser.Parameters.AddWithValue("@keyWord", txtPassWord.Text.Trim());
+                SqlDataReader reader = GetUser.ExecuteReader();
+                if (reader.Read())
+                {
+
+                    Response.Redirect("WelcomeEC.aspx");
+                   
+                }
+                else
+                {
+                    //Response.Write("User no exists");
+                    Label1.Visible = true;
+                    Label1.Text = "User doesn´t exist, please check your account";
+                    txtUser.Text = "";
+
+
+                }
+            }
+            catch (Exception m)
+            {
+
+                Label2.Text = m.Message;
+            }
+            
+
+        }
+    }
+}
---
+++ Duban/DBCommerceLogin/DBCommerceLogin/WelcomeEC.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DBCommerceLogin
+{
+    public partial class WelcomeEC : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
