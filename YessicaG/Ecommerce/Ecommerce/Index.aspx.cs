9b16771395b8d84fe1cfdb00451aad68b4151257
YESSICA ELEANA GOMEZ SANCHEZ <YESSICA ELEANA GOMEZ SANCHEZ>
2016-03-31 23:36:15 -0500

    # 004 Login of the Ecommerce

---
+++ YessicaG/Ecommerce/Ecommerce/Index.aspx.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web.Security;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Ecommerce
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            LblMesagge.Visible = false;
+        }
+
+        protected void ButSubmit_Click(object sender, EventArgs e)
+        {
+            string UserName = TextUser.Text;
+
+            using (SqlConnection connection = new SqlConnection(@"Data Source = ADMINISTRATOR1\SQLEXPRESS; Integrated Security = SSPI; Initial Catalog = Ecommerce"))
+            {
+                using (SqlCommand cmd = new SqlCommand("GetSingleUserEcommerce", connection))
+                {
+
+                    string aux = Function_String(TextPasword.Text);
+
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@userName", SqlDbType.VarChar).Value = UserName;
+                    cmd.Parameters.Add("@passwordUser", SqlDbType.VarChar).Value = aux;
+                    connection.Open();
+
+                    try
+                    {
+                        SqlDataReader rdr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+
+
+                        if (rdr.HasRows)
+                        {
+                            LblMesagge.Text = "Your Conecction was successful";
+                        }
+                        else
+                        {
+                            LblMesagge.Visible = true;
+                            LblMesagge.Text = "Your username or password are incorrect";
+                            Server.Transfer("Default.aspx", true);
+                            // Response.Redirect("Default.apx");
+                        }
+
+                    }
+                    catch (Exception)
+                    {
+
+                        LblMesagge.Text = "There was no response from the server , contact the administrator";
+                        Server.Transfer("Default.aspx", true);
+                        // Response.Redirect("Default.apx");
+                    }
+
+                    finally
+                    {
+                        connection.Close();
+
+                    }
+                }
+            }
+
+
+        }
+
+        public string Function_String(string strkeyword)
+        {
+            MD5 md5 = System.Security.Cryptography.MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(strkeyword);
+            byte[] hash = md5.ComputeHash(inputBytes);
+            LblMesagge.Visible = true;
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ YessicaG/Ecommerce/Ecommerce/Login/Login.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="Ecommerce.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 300px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <table align="center" class="auto-style1">
+            <tr>
+                <td colspan="2">Login</td>
+            </tr>
+            <tr>
+                <td>User</td>
+                <td>
+                    <asp:TextBox ID="TextUser" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td>
+                    <asp:TextBox ID="TextPasword" runat="server" TextMode="Password"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <asp:Button ID="ButSubmit" runat="server" Text="Submit" OnClick="ButSubmit_Click" />
+                    <br />
+                    <asp:Label ID="LblMesagge" runat="server" Text="LblMessage" Visible="False"></asp:Label>
+                </td>
+            </tr>
+        </table>
+    
+    </div>
+    </form>
+</body>
+</html>
